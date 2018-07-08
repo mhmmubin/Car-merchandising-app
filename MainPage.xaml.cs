2750a0493a93457a2023f1d5e72f052c5010ac41
Mobashsher Mubin <36401740+mhmmubin@users.noreply.github.com>
2018-07-08 20:28:00 +0930

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -18,16 +18,7 @@ using Windows.UI.Xaml.Navigation;
 
 namespace AssignmentP3
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-
-        /*
-         Developer: MUBIN
-         Project: Assignment Part3
-         Project Details: Vehicle final cost calculation including Insurance.
-         Date: 12-11-17
-         */
+   
 
     public sealed partial class MainPage : Page
     {
